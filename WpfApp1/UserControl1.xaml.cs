3c4ded27ee3b16350498ed8a2c186d25761e0de3
Fredrik Andersson <git@git.glyph.se>
2023-01-11 15:17:37 +0100

    Add minimal code that causes the issue

---
+++ WpfApp1/UserControl1.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Controls;
+
+namespace WpfApp1
+{
+	/// <summary>
+	/// Interaction logic for UserControl1.xaml
+	/// </summary>
+	internal partial class UserControl1 : UserControl
+	{
+		// This constructor causes the issues
+		internal UserControl1()
+		{
+			InitializeComponent();
+		}
+	}
+}
